e449e9edd5fbdf44737b57b378e656f3f31e5620
Abdur-Rahman-Argon <abdurrahman.me7@gmail.com>
2023-01-29 01:46:07 +0600

    add to data base

--- index.js
+++ index.js
@@ -9,6 +9,42 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tv6nj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client
+      .db("doctors_portal")
+      .collection("servises");
+
+    const bookingCollection = client.db("doctors_portal").collection("booking");
+
+    const userCollection = client.db("doctors_portal").collection("user");
+
+    const doctorsCollection = client.db("doctors_portal").collection("doctors");
+
+    const paymentsCollection = client
+      .db("doctors_portal")
+      .collection("payments");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("ser is running successfully");
 });
