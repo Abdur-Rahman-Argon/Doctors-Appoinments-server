bf4098b420533a01bc62e049f432635907368657
Abdur-Rahman-Argon <abdurrahman.me7@gmail.com>
2023-01-29 01:41:06 +0600

    doctors server setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("ser is running successfully");
+});
+
+app.listen(port, () => {
+  console.log("server is connected on port", port);
+});
