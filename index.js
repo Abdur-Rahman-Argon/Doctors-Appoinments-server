a9836cd6b2f8d6bfe17aa412cb3e8aeba7677997
Abdur-Rahman-Argon <abdurrahman.me7@gmail.com>
2023-01-29 02:08:04 +0600

    booking payment update

--- index.js
+++ index.js
@@ -181,10 +181,49 @@ async function run() {
       };
       const exists = await bookingCollection.findOne(query);
       if (exists) {
-        return res.send({ success: false, booking: exists });
+        const message = `You already have a booking on ${booking.date}`;
+        return res.send({
+          acknowledged: false,
+          message,
+          success: false,
+          booking: exists,
+        });
       }
       const result = await bookingCollection.insertOne(booking);
       res.send({ success: true, result });
+    });
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const booking = req.body;
+      const price = booking.price;
+      const amount = price * 100;
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency: "usd",
+        amount: amount,
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      const result = await paymentsCollection.insertOne(payment);
+      const id = payment.bookingId;
+      const filter = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId,
+        },
+      };
+      const updatedResult = await bookingCollection.updateOne(
+        filter,
+        updatedDoc
+      );
+      res.send(result);
     });
   } finally {
   }
